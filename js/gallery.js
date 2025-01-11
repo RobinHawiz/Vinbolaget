491c538d2b08bc2c67dd310ee17f3e1a67538289
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-01-11 14:19:06 +0100

    Remove .slice(0, .5) from variable productionName

--- js/gallery.js
+++ js/gallery.js
@@ -7,7 +7,7 @@ let galleryButtons = document.querySelectorAll(
 );
 
 let path = window.location.pathname;
-let productName = path.split("/").pop().slice(0, -5);
+let productName = path.split("/").pop();
 let totImages = galleryItemsImages.length;
 let totVideos = galleryItemsVideos.length;
 
